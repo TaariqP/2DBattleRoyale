208273df36e7a6718738ea1ec5e5e85361eeb09d
Matthew Fennell <matthew.robert.fennell@gmail.com>
2018-01-27 13:41:57 +0000

    Added basic methods for Map

--- src/Map/Map.java
+++ src/Map/Map.java
@@ -4,12 +4,13 @@ import Entity.EntityType;
 
 public class Map {
   final int mapSize;
+  Tile[][] map;
 
   Map(int mapSize) {
     this.mapSize = mapSize;
   }
 
   public void moveEntityToPosition(Tile initial, Tile next) {
-
+    next.setEntityOnTile(initial.getEntityOnTile());
   }
 }
--- src/Map/Tile.java
+++ src/Map/Tile.java
@@ -1,12 +1,29 @@
 package Map;
 
-public class Tile {
-  boolean containsEntity;
-  int x;
-  int y;
+import javax.swing.text.html.parser.Entity;
 
-  Tile(int x, int y) {
+public class Tile {
+  private int x;
+  private int y;
+  private TileType tileType;
+  private Entity entityOnTile;
+
+  Tile(int x, int y, TileType tileType, Entity entityOnTile) {
     this.x = x;
     this.y = y;
+    this.tileType = tileType;
+    this.entityOnTile = entityOnTile;
+  }
+
+  public final Entity getEntityOnTile() {
+    return entityOnTile;
+  }
+
+  public final TileType getTileType() {
+    return tileType;
+  }
+
+  public final Tile setEntityOnTile(Entity newEntity) {
+    return new Tile(x, y, tileType, newEntity);
   }
 }
