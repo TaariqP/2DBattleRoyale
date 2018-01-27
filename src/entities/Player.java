f2f4bbd3c1ec74408c9d71ce6838ea77e42af205
Taariq <tp1717@ic.ac.uk>
2018-01-27 13:22:20 +0000

    test

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -2,4 +2,16 @@ package entities;
 
 public class Player {
 
+  private final String playerName;
+  private final int ID;
+  private int health;
+  private Coordinate playerPosition;
+  
+  public Player(String playerName, int ID) {
+    this.playerName = playerName;
+    this.ID = ID;
+  }
+
+
+
 }
