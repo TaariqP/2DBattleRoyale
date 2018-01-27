d6d971b535a72acb118e22be85de7494479de892
Tiger Cross <tc2017@ic.ac.uk>
2018-01-27 23:33:23 +0000

    "randomised the lakes and bushes"

--- src/Map/Tile.java
+++ src/Map/Tile.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 import javax.imageio.ImageIO;
-import javax.swing.text.html.parser.Entity;
 
 public class Tile {
 
@@ -32,19 +31,18 @@ public class Tile {
   }
 
   public void generateImage() {
-    if (tileType == TileType.GRASS) {
-      Random number = new Random();
-      int choice = number.nextInt(4) + 1;
-      String input = "PNG/Tiles/tile_0" + Integer.toString(choice) + ".png";
-      File location = new File(input);
+    Random number = new Random();
+    int choice = number.nextInt(tileType.getNumberOfImages()) +
+        tileType.getStartingIndex();
+    String input = "PNG/Tiles/tile_" + Integer.toString(choice) + ".png";
+    File location = new File(input);
 
-      image = null;
-      try {
-        image = ImageIO.read(location);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+    image = null;
+    try {
+      image = ImageIO.read(location);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
   }
+
 }
--- src/Map/TileType.java
+++ src/Map/TileType.java
@@ -1,15 +1,31 @@
 package Map;
 
 public enum TileType {
-  GRASS("grass "), WATER("water "), WALL("wall "), ERROR("error ");
+  GRASS("grass ",4,1), WATER("water ",2,5), WALL("wall ",1,7), ERROR("error"
+      + " ",1,8);
 
   private String representation;
+  private int numberOfImages;
+  private int startingIndex;
 
-  TileType(String representation){
+  TileType(String representation, int numberOfImages, int startingIndex){
     this.representation = representation;
+    this.numberOfImages = numberOfImages;
+    this.startingIndex = startingIndex;
   }
 
   public String getRepresentation(){
     return representation;
   }
+
+  public int getNumberOfImages() {
+    return numberOfImages;
+  }
+
+  public int getStartingIndex() {
+    return startingIndex;
+  }
+
 }
+
+
