9a9c7614e1c3e978414c713b0b6cbbb926410cac
Matthew Fennell <matthew.robert.fennell@gmail.com>
2018-01-27 13:32:18 +0000

    Initial implementation of Map package

---
+++ src/Map/Map.java
@@ -0,0 +1,15 @@
+package Map;
+
+import Entity.EntityType;
+
+public class Map {
+  final int mapSize;
+
+  Map(int mapSize) {
+    this.mapSize = mapSize;
+  }
+
+  public void moveEntityToPosition(Tile initial, Tile next) {
+
+  }
+}
