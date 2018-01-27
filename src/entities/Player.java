62d0e476b6114cd8078056c239e15a5d044d5c36
Taariq <tp1717@ic.ac.uk>
2018-01-27 13:29:42 +0000

    two

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -2,16 +2,34 @@ package entities;
 
 public class Player {
 
+  private final String playerType;
   private final String playerName;
   private final int ID;
   private int health;
   private Coordinate playerPosition;
-  
-  public Player(String playerName, int ID) {
+
+  public Player(String playerName, int ID, String playerType) {
     this.playerName = playerName;
     this.ID = ID;
   }
 
+  public String getPlayerType() {
+    return playerType;
+  }
 
+  public void setPlayerPosition(Coordinate playerPosition) {
+    this.playerPosition = playerPosition;
+  }
 
+  public void setHealth(int health) {
+    this.health = health;
+  }
+
+  public Coordinate getPlayerPosition() {
+    return playerPosition;
+  }
+
+  public int getHealth() {
+    return health;
+  }
 }
