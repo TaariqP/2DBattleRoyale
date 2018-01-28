12bbb07ebceba11489282eac2122fff36c031c7b
Tiger Cross <tc2017@ic.ac.uk>
2018-01-28 09:08:24 +0000

    optimised maps for spawning

--- src/Map/GenerateRandomMaps.java
+++ src/Map/GenerateRandomMaps.java
@@ -3,9 +3,9 @@ package Map;
 public class GenerateRandomMaps {
 
   public static void main(String[] args) {
-    for (int i = 0; i < 10; i++) {
-      //MapGeneration mapGeneration = new MapGeneration("Maps/" + i + ".txt");
-      System.out.println(i);
-    }
+    //for (int i = 0; i < 10; i++) {
+      MapGeneration mapGeneration = new MapGeneration("Maps/" + 6 + ".txt");
+      System.out.println(6);
+    //}
   }
 }
